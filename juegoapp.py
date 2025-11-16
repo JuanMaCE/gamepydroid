cc6972776ef34445034a2f8a75057f09434e2663
Juan Marcos Cortés Enriquez <124099699+JuanMaCE@users.noreply.github.com>
2025-11-15 23:24:22 -0600

    se arreglo el error de carga en android

--- juegoapp.py
+++ juegoapp.py
@@ -1,3 +1,16 @@
+from kivy.config import Config
+
+# 🔥 Estas dos líneas solucionan el problema de que Android arranque "chiquito"
+Config.set('graphics', 'fullscreen', 'auto')
+Config.set('graphics', 'resizable', True)
+
+# Opcional pero recomendado para evitar comportamiento raro al reabrir
+Config.set('kivy', 'pause_on_resume', 0)
+
+# -- Después ya puedes importar Kivy --
+from kivy.core.window import Window
+Window.fullscreen = True      # Se verá fullscreen tanto en PC como en Android
+
 from kivy.app import App
 from screencontroler import ScreenController
 
